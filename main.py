f834cfbd9ce3fd8d788b1c45b057b00ad5d39dc6
Marian Bajan <marianbajan1502@yahoo.com>
2024-06-14 13:17:56 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    a = 10
+    b = 20
+
+    # TODO
+    # res1 = add(a, b) - mircea
+    # res2 = multiply(a, b) - karina
+
+    print(a)
+    print(b)
+    print("a + b = ?")
+    print("a * b = ?")
+
+
+if __name__ == "__main__":
+    main()
