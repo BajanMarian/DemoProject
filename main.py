c6b3263756a16b1a197c609ef3e96cae5feb8b82
Marian Bajan <marianbajan1502@yahoo.com>
2024-06-14 13:30:17 +0300

    implemented multiply function

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+def multiply(a, b):
+    return a * b
+
+
 def main():
     a = 10
     b = 20
 
     # TODO
     # res1 = add(a, b) - mircea
-    # res2 = multiply(a, b) - karina
 
     print(a)
     print(b)
     print("a + b = ?")
-    print("a * b = ?")
+    print("a * b = ", multiply(a, b))
 
 
 if __name__ == "__main__":
