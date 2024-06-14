67aea6c73052f2765d721ced0c82b612b1662b63
Marian Bajan <marianbajan1502@yahoo.com>
2024-06-14 13:24:28 +0300

    implemented add function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+def add(a, b):
+    return a + b
+
+
 def main():
     a = 10
     b = 20
 
     # TODO
-    # res1 = add(a, b) - mircea
     # res2 = multiply(a, b) - karina
 
     print(a)
     print(b)
-    print("a + b = ?")
+    print("a + b = ", add(a, b))
     print("a * b = ?")
 
 
